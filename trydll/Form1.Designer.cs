b98d3413b62bcba8258533f72950f69c73e87b55
Thornhill-GYL <Thornhill-GYL@outlook.com>
2018-11-25 00:14:52 +0800

    the c# files to using c++ dll

---
+++ trydll/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace trydll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.确认ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.输出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(282, 244);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.确认ToolStripMenuItem,
+            this.输出ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(636, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 确认ToolStripMenuItem
+            // 
+            this.确认ToolStripMenuItem.Name = "确认ToolStripMenuItem";
+            this.确认ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.确认ToolStripMenuItem.Text = "确认";
+            this.确认ToolStripMenuItem.Click += new System.EventHandler(this.确认ToolStripMenuItem_Click);
+            // 
+            // 输出ToolStripMenuItem
+            // 
+            this.输出ToolStripMenuItem.Name = "输出ToolStripMenuItem";
+            this.输出ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.输出ToolStripMenuItem.Text = "输出";
+            this.输出ToolStripMenuItem.Click += new System.EventHandler(this.输出ToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(400, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 459);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 确认ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 输出ToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ trydll/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+namespace trydll
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("F:\\code\\c#\\trydll\\Release\\dlltest.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int add(int a, int b);
+        [DllImport("F:\\code\\c#\\trydll\\Release\\dlltest.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void lex();
+        public Form1()
+        {
+          // 
+            InitializeComponent();
+        }
+       
+        private void 确认ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            richTextBox1.LoadFile("F:\\shuchu.txt", RichTextBoxStreamType.PlainText);
+        }
+
+        private void 输出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lex();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            sum = add(2,3);
+            if (sum == 5)
+            {
+                label1.Text = "ok";
+            }
+            else
+            {
+                label1.Text = "failed";
+            }
+        }
+    }
+}
